613ccaf79a6f86f73b9159f4fed4737cf7dd7b5c
bearlord <565364226@hotmail.com>
2025-09-30 12:07:24 +0800

    [update]

---
+++ src/Plugins/Topic/Driver/Db/DbDriver.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Yew\Plugins\Topic\Driver\Db;
+
+use Yew\Plugins\Topic\Driver\DriverInterface;
+
+class DbDriver implements DriverInterface
+{
+    public function addSub(string $topic, string $uid)
+    {
+        // TODO: Implement addSub() method.
+    }
+
+    public function removeSub(string $topic, string $uid)
+    {
+        // TODO: Implement removeSub() method.
+    }
+
+    public function hasTopic(string $topic, string $uid): bool
+    {
+        // TODO: Implement hasTopic() method.
+    }
+
+    public function delTopic(string $topic)
+    {
+        // TODO: Implement delTopic() method.
+    }
+
+    public function clearFdSub(int $fd)
+    {
+        // TODO: Implement clearFdSub() method.
+    }
+
+    public function clearUidSub(string $uid)
+    {
+        // TODO: Implement clearUidSub() method.
+    }
+
+    public function pub(string $topic, $data, ?array $excludeUidList = [])
+    {
+        // TODO: Implement pub() method.
+    }
+
+
+}
--- src/Plugins/Topic/Driver/DriverStrategy.php
+++ src/Plugins/Topic/Driver/DriverStrategy.php
@@ -2,7 +2,63 @@
 
 namespace Yew\Plugins\Topic\Driver;
 
+use Yew\Plugins\Topic\Driver\Db\DbDriver;
+use Yew\Plugins\Topic\Driver\Memory\MemoryDriver;
+
 class DriverStrategy
 {
+
+    private DriverInterface $strategy;
+
+
+    public function __construct(?string $type = null)
+    {
+        switch ($type) {
+            case "db":
+                $this->strategy = new DbDriver();
+                break;
+
+            case "memory":
+            default:
+                $this->strategy = new MemoryDriver();
+        }
+    }
+
+
+    public function addSub(string $topic, string $uid)
+    {
+        $this->strategy->addSub($topic, $uid);
+    }
+
+    public function removeSub(string $topic, string $uid)
+    {
+        $this->strategy->removeSub($topic, $uid);
+    }
+
+    public function hasTopic(string $topic, string $uid): bool
+    {
+        return $this->strategy->hasTopic($topic, $uid);
+    }
+
+    public function delTopic(string $topic)
+    {
+        $this->strategy->delTopic($topic);
+    }
+
+    public function clearFdSub(int $fd)
+    {
+        $this->strategy->clearFdSub($fd);
+    }
+
+    public function clearUidSub(string $uid)
+    {
+        $this->strategy->clearUidSub($uid);
+    }
+
+    public function pub(string $topic, $data, ?array $excludeUidList = [])
+    {
+        $this->strategy->pub($topic, $data, $excludeUidList);
+    }
+
 
 }
