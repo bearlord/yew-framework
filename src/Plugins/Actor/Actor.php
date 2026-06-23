016d71e28992cdb64836d0a88bf0c8a50f00b9e9
bearlord <565364226@hotmail.com>
2026-06-23 16:01:21 +0800

    [update]

--- src/Plugins/Actor/Actor.php
+++ src/Plugins/Actor/Actor.php
@@ -328,7 +328,7 @@ abstract class Actor
             foreach ($this->timerIds as $timerId) {
                 $this->clearTimer($timerId);
             }
-            $this->debug(sprintf("Actor %s"s all timer cleared", $this->getName()));
+            $this->debug(sprintf("Actor %s's all timer cleared", $this->getName()));
         }
         return true;
     }
