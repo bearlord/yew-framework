20703ac613305ace7391ab783b7a6fc784903bb4
bearlord <565364226@hotmail.com>
2026-07-28 15:54:11 +0800

    [update]

---
+++ src/Mqtt/Tools/ProtocolLevel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Yew\Mqtt\Tools;
+
+class ProtocolLevel
+{
+    const PROTOCOL_LEVEL_V3_1 = 3;
+
+    const PROTOCOL_LEVEL_V3_1_1 = 4;
+
+    const PROTOCOL_LEVEL_V5 = 5;
+
+    /**
+     * @return string[]
+     */
+    public static function protocolLevels(): array
+    {
+        return [
+            self::PROTOCOL_LEVEL_V3_1 => 'v3.1',
+            self::PROTOCOL_LEVEL_V3_1_1 => 'v3.1.1',
+            self::PROTOCOL_LEVEL_V5 => '5.0'
+        ];
+    }
+
+    /**
+     * @param int $level
+     * @return string
+     */
+    public function getProtocolLevel(int $level)
+    {
+        $all = self::protocolLevels();
+        if (isset($all[$level])) {
+            return $all[$level];
+        }
+        return 'unknown';
+    }
+}
